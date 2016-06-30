86bdc8bdf5f0f5c7fe4797e74d0e4a56d298be9f
Susie O'Brien <email": null>
2016-06-30 12:55:25 -0500

    get users working

---
+++ sinatra-skeleton-mvc-challenge-master/app/controllers/user.rb
@@ -0,0 +1,35 @@
+get '/users/register' do
+  erb :"/users/register"
+end
+
+post '/users/new' do
+  @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user
+      redirect '/'
+    else
+      erb :"/users/register"
+    end
+end
+
+get '/users/login' do
+  erb :"/users/login"
+end
+
+
+post '/users/login' do
+  p params[:user][:username]
+  @user = User.find_by(username: params[:user][:username])
+  p @user
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :"/users/login"
+  end
+end
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
---
+++ sinatra-skeleton-mvc-challenge-master/db/migrate/20160630114336_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, { null: false, uniqueness: true }
+      t.string :username, { null: false, uniqueness: true }
+      t.string :password_hash, { null: false }
+      t.timestamps(null: false)
+    end
+  end
+end
