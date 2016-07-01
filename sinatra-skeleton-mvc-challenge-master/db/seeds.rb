9f062e1a01fac805ac0a9c5a9ec934431817f3e0
Susie O'Brien <email": null>
2016-07-01 13:48:48 -0500

    Fix bg

--- sinatra-skeleton-mvc-challenge-master/db/seeds.rb
+++ sinatra-skeleton-mvc-challenge-master/db/seeds.rb
@@ -2,6 +2,10 @@ User.create!(email: "susie",
   username: "Susie",
   password: "password")
 
+User.create!(email: "laura",
+  username: "laura",
+  password: "laura")
+
 5.times { Cup.create!(user_id: 1, created_at: "2016-06-29 18:55:39.955234") }
 
 7.times { Cup.create!(user_id: 1, created_at: "2016-06-28 18:55:39.955234") }
